91acdb5c26d3ceab7dec44fab650a2a454537e92
rkorzen <korzeniewski@gmail.com>
2024-12-02 15:39:50 +0100

    cwiczenie dekorator

--- rozwiazania/zamowienia.py
+++ rozwiazania/zamowienia.py
@@ -1,4 +1,6 @@
-
+from rich import print
+from tabulate import tabulate
+from typing import Callable
 
 orders = [
     {"product": "Laptop", "type": "online", "category": "electronics", "amount": 1200},
@@ -10,8 +12,10 @@ orders = [
 
 
 def tax_factory(rate):
-    def calculate_tax(amount):
-        return amount * rate
+    def calculate_tax(order):
+        order["tax_amount"] = order["calculated_amount"] * rate
+        order["tax_rate"] = rate
+        return order
     return calculate_tax
 
 tax_functions = {
@@ -26,14 +30,39 @@ tax_functions = {
 # dla vip - 10% rabatu ale tylko dla zamówień powyżej 100zł
 # dla store - 5% rabatu
 
+def process_online_order(order: dict[str, str | int]) -> dict[str, str | int]:
+    order["calculated_amount"] = order["amount"] + 10
+    order["discount"] = 10
+    return order
 
-def process_order(order, tax_calculator):
+def process_vip_order(order):
+    if order["amount"] > 100:
+        order["calculated_amount"] = order["amount"] * 0.9
+        order["discount"] =  -(order["amount"] - order["calculated_amount"])
+    else:
+        order["calculated_amount"] = order["amount"]
+        order["discount"] = 0
+    return order
+
+def process_store_order(order):
+    order["calculated_amount"] = order["amount"] * 0.95
+    order["discount"] =  -(order["amount"] - order["calculated_amount"])
+    return order
+
+order_processors = {
+    "online": process_online_order,
+    "vip": process_vip_order,
+    "store": process_store_order,
+}
+
+
+def process_order(order: dict, tax_calculator: Callable, order_processor: Callable) -> dict:
     
     # tutaj trzeba zaaplikowac logike zamowienia:
+    order = order_processor(order)
 
-    
     # logika obliczania podatku:
-    order["tax_amount"] = tax_calculator(order["amount"])
+    order = tax_calculator(order)
     
     return order
 
@@ -51,9 +80,10 @@ def process_orders(orders):
 
     for order in orders:
         tax_calculator = tax_functions[order["category"]]
-        
-        processed_order = process_order(order, tax_calculator)
-    
+        order_processor = order_processors[order["type"]]
+
+        processed_order = process_order(order, tax_calculator, order_processor)
+
 
         summary["total_value"] += processed_order["amount"]
         summary["total_tax"] += processed_order["tax_amount"]
@@ -61,5 +91,12 @@ def process_orders(orders):
 
     print("Podsumowanie zamówień:")
     print(summary)
+    print(display_summary_table(orders))
+
+
+def display_summary_table(orders):
+
+    return tabulate(orders, headers="keys", tablefmt="grid")
+
 
 process_orders(orders)
